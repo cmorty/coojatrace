a55b0ebe837db65fdb3a745406b9c48ada3d6e11
Florian Lukas <florian.lukas@gmail.com>
2011-09-14 14:31:22 +0200

    CoojaTestPlugin: - GUI erstellung in createGUI() ausgelagert - deaktivieren moeglich - Interpreter wird immer neu erstellt - Classloader-Check fuer Reload-Warnung eingebaut - Speichern und Laden aus Config moeglich - Result Textfeld entfernt (-> Console bzw. TODO)

--- scala/se/sics/cooja/coojatest/CoojaTest.scala
+++ scala/se/sics/cooja/coojatest/CoojaTest.scala
@@ -2,12 +2,14 @@ package se.sics.cooja.coojatest
 
 import se.sics.cooja._
 
-import java.util._
+import java.util.{List => _, _}
 import java.io._
 
 import javax.swing._
-import java.awt._
+import java.awt.{List => _, _}
 import java.awt.event._
+
+import scala.collection.JavaConversions._
 
 import magicsignals._
 import operators._
@@ -23,29 +25,56 @@ import mspwrappers._
 class CoojaTestPlugin(sim: Simulation, gui: GUI) extends VisPlugin("CoojaTest", gui, false) {
   val logger = org.apache.log4j.Logger.getLogger(this.getClass)
   
-  // Aufteilung
-  val grid = new GridLayout(0,2)
-  setLayout(grid)
-  
-  // Beschriftung
-  val scriptLabel = new JLabel("Script:")
-
   // Code Textfeld
   val scriptCode = new JTextArea()
 
-  // Ergebnis Textfeld
-  val scriptResult = new JTextArea()
-  val pwriter = new PrintWriter(new Writer () { 
-    def close {}
-    def flush {}
-    def write(cbuf: Array[Char],  off: Int, len: Int) {
-      scriptResult.append(new String(cbuf, off, len));
+  // Run Button
+  val scriptButton = new JButton("Activate");
+
+  if(GUI.isVisualized) createGUI()
+
+  def createGUI() {
+    // Aufteilung
+    val grid = new GridLayout(0,2)
+    setLayout(grid)
+    
+    // Beschriftung
+    val scriptLabel = new JLabel("Script:")
+
+    scriptButton.addActionListener(new ActionListener() {
+      def actionPerformed(e: ActionEvent) {
+        if(isActive)
+          deactivate()  
+        else
+          activate()
+      }
+    })
+
+    //add(scriptLabel)
+    add(scriptButton)
+    add(new JScrollPane(scriptCode))
+    
+    setSize(500,500)
+  
+    // Reload notwendig?
+    if(sim.getMotes.exists { _.getClass.getClassLoader != this.getClass.getClassLoader } ) {
+      val s1 = "Reload"
+      val s2 = "Ignore"
+      val options = Array[Object](s1, s2)
+      val n = JOptionPane.showOptionDialog(
+                GUI.getTopParentContainer(),
+                "The CoojaTest plugin was loaded after other plugins. This can load to classloader inconsistencies.\nDo you want to reload the simulation?",
+                "Reload simulation?", JOptionPane.YES_NO_OPTION,
+                JOptionPane.QUESTION_MESSAGE, null, options, s1);
+      if (n == JOptionPane.YES_OPTION) {
+        sim.getGUI.reloadCurrentSimulation(false)
+      }
     }
-  })
-  scriptResult.setEditable(false)
+  }
+  
   
   // Scala Interpreter
-  lazy val interpreter = {
+  def createInterpreter() = {
     import scala.tools.nsc._
     
     val settings = new Settings()
@@ -67,75 +96,80 @@ class CoojaTestPlugin(sim: Simulation, gui: GUI) extends VisPlugin("CoojaTest", 
     val dynamicLibs = classloader.getURLs.map(_.toString.replace("file:", "")).toList
     settings.bootclasspath.value = (coojaLibs ::: dynamicLibs).mkString(":")
     
-    new Interpreter(settings, pwriter)
+    new Interpreter(settings, new PrintWriter(System.out, true))
   }
-  
-  // Run Button
-  val scriptButton = new JButton("run");
-  scriptButton.addActionListener(new ActionListener() {
-    def actionPerformed(e: ActionEvent) {
-      pwriter.flush()
-      interpreter.reset()
-      rules.reset()
 
-      interpreter.interpret("""
-        import se.sics.cooja._
-        import se.sics.cooja.interfaces._
-        import reactive._
-        import se.sics.cooja.coojatest.wrappers.Conversions._
-        import se.sics.cooja.coojatest.interfacewrappers.Conversions._
-        import se.sics.cooja.coojatest.magicsignals.MagicSignals._
-        import se.sics.cooja.coojatest.rules._
-        import se.sics.cooja.coojatest.operators._
+  private var interpreter: scala.tools.nsc.Interpreter = null
 
-        import se.sics.cooja.coojatest.contikiwrappers._
-        se.sics.cooja.coojatest.contikiwrappers.register()
-        import se.sics.cooja.coojatest.mspwrappers._
-        se.sics.cooja.coojatest.mspwrappers.register()
-      """)
-      
-      interpreter.bind("sim", sim.getClass.getName, sim)
-      
-      interpreter.interpret("""
-        implicit val _simulation = sim
-        implicit val _observing = new Observing {}
-        implicit val _dyndeplog = new se.sics.cooja.coojatest.magicsignals.DynDepLog
-      """)
-	
-      scriptResult.setText("")
-      
-      val res = interpreter.interpret(scriptCode.getText())
-      logger.debug(res)
-      if(res == scala.tools.nsc.InterpreterResults.Success) {
-        scriptResult.setForeground(Color.BLACK)
-      }
-      else {
-        scriptResult.setForeground(Color.RED)
-      }
-        
+  private var active = false
+  def isActive = active
+
+  def activate() {
+    interpreter = createInterpreter()
+
+    interpreter.interpret("""
+      import se.sics.cooja._
+      import se.sics.cooja.interfaces._
+      import reactive._
+      import se.sics.cooja.coojatest.wrappers.Conversions._
+      import se.sics.cooja.coojatest.interfacewrappers.Conversions._
+      import se.sics.cooja.coojatest.magicsignals.MagicSignals._
+      import se.sics.cooja.coojatest.rules._
+      import se.sics.cooja.coojatest.operators._
+
+      import se.sics.cooja.coojatest.contikiwrappers._
+      se.sics.cooja.coojatest.contikiwrappers.register()
+      import se.sics.cooja.coojatest.mspwrappers._
+      se.sics.cooja.coojatest.mspwrappers.register()
+    """)
+    
+    interpreter.bind("sim", sim.getClass.getName, sim)
+    
+    interpreter.interpret("""
+      implicit val _simulation = sim
+      implicit val _observing = new Observing {}
+      implicit val _dyndeplog = new se.sics.cooja.coojatest.magicsignals.DynDepLog
+    """)
+    
+    val res = interpreter.interpret(scriptCode.getText())
+    logger.debug("Interpreting script: " + res)
+    if(res == scala.tools.nsc.InterpreterResults.Success) {
+      scriptButton.setText("Reset")
+      active = true
     }
-  })
-
-  //add(scriptLabel)
-  add(scriptButton)
-  add(new JScrollPane(scriptCode))
-  add(new JScrollPane(scriptResult))
-  
-  setSize(500,500)
-  
-  // Reload notwendig?
-  if(true) { // TODO
-    val s1 = "Reload"
-    val s2 = "Ignore"
-    val options = Array[Object](s1, s2)
-    val n = JOptionPane.showOptionDialog(
-              GUI.getTopParentContainer(),
-              "The CoojaTest plugin was loaded after other plugins. This can load to classloader inconsistencies.\nDo you want to reload the simulation?",
-              "Reload simulation?", JOptionPane.YES_NO_OPTION,
-              JOptionPane.QUESTION_MESSAGE, null, options, s1);
-    if (n == JOptionPane.YES_OPTION) {
-      sim.getGUI.reloadCurrentSimulation(false)
+    else {
+      //if(GUI.showErrorDialog(this, "Scala Compilation Error", new Exception(""), true)) activate()
     }
+  }
+
+  def deactivate() {
+    interpreter.reset() // necessary?
+    interpreter = null
+
+    scriptButton.setText("Activate")
+
+    active = false
+  }
+
+  override def getConfigXML(): java.util.Collection[org.jdom.Element] = {
+    val script = new org.jdom.Element("script").setText(scriptCode.getText)
+    val active = new org.jdom.Element("active").setText(isActive.toString)
+    List(script, active)
+  }
+
+  override def setConfigXML(configXML: java.util.Collection[org.jdom.Element], visAvailable: Boolean): Boolean = {
+    for(e <- configXML) {
+      if(e.getName == "script" && !e.getText.isEmpty) scriptCode.setText(e.getText)
+      if(e.getName == "active" && GUI.isVisualized) activate()
+    }
+
+    if(!GUI.isVisualized) {
+      activate()
+      //sim.setDelayTime(0)
+      //sim.startSimulation()
+    }
+
+    true
   }
   
   override def closePlugin() { 
