e689334e7d8921cff021f06b8d9f0eff6069536a
Florian Lukas <florian.lukas@gmail.com>
2011-09-14 14:29:28 +0200

    Sowohl Signals als auch EventStreams koennen jetzt fuer log() und assert() verwendet werden

--- scala/se/sics/cooja/coojatest/Rules.scala
+++ scala/se/sics/cooja/coojatest/Rules.scala
@@ -15,13 +15,19 @@ import se.sics.cooja._
 
 package object rules {
   private var assertions = List[Assertion]()
-  def assert(s:Signal[Boolean])(implicit sim: Simulation) { 
+  def assert(s: Signal[Boolean])(implicit sim: Simulation) { 
     assertions ::= new Assertion(s, sim)
+  }
+  def assert(es: EventStream[Boolean])(implicit sim: Simulation) { 
+    assertions ::= new Assertion(es.hold(true), sim)
   }
   
   private var logrules = List[LogRule]()
   def log(logdest: LogDestination, s: Signal[_]*)(implicit sim: Simulation) { 
     logrules ::= new LogRule(logdest, sim, s.toList) 
+  }
+  def log(logdest: LogDestination, es: EventStream[_])(implicit sim: Simulation) { 
+    logrules ::= new LogRule(logdest, sim, es) 
   }
 
   def reset() {
@@ -47,7 +53,7 @@ package rules {
 
 
   trait LogDestination {
-    def log(time: Long, values: List[String]): Unit
+    def log(time: Long, values: List[_]): Unit
   }
 
   case class LogFile(file: String, sep: String = "\t")(implicit sim: Simulation) extends LogDestination {
@@ -58,7 +64,7 @@ package rules {
       }
     })
 
-    def log(time: Long, values: List[String]) {
+    def log(time: Long, values: List[_]) {
       stream.println(time + sep + values.mkString(sep))
     }
   }
@@ -82,19 +88,28 @@ package rules {
     window.show()
     sim.getGUI.getDesktopPane.add(window)
 
-    def log(time: Long, values: List[String]) {
+    def log(time: Long, values: List[_]) {
       val v = new java.util.Vector[Object](values.length + 1)
       v.addElement((time/1000.0).asInstanceOf[AnyRef])
-      values foreach v.addElement
+      for(x <- values) v.addElement(x.asInstanceOf[AnyRef])
       model.addRow(v)
     }
   }
 
-  class LogRule(val dest: LogDestination, val sim: Simulation, val values: List[Signal[_]]) extends Rule {
-    values.tail.foldLeft(values.head) {
-      case (combined, signal) => signal.flatMap(s => combined)
-    }.foreach { 
-      c => dest.log(new RichSimulation(sim).time, values.map(_.now.toString))
+  class LogRule(val dest: LogDestination, val sim: Simulation, val es: EventStream[_]) extends Rule {
+    def this(dest: LogDestination, sim: Simulation, values: List[Signal[_]]) = this(
+      dest, sim, values.tail.foldLeft(values.head) {
+        case (combined, signal) => signal.flatMap(s => combined)
+      }.map {
+        x => values.map(_.now.toString)
+      }.change
+    )
+
+    for(e <- es) {
+      dest.log(new RichSimulation(sim).time, e match {
+        case list: List[_] => list
+        case event: Any => List(event) 
+      })
     }
   }
 }
